e650e2187d91d85c95777cde6f5ac6d440dba8e9
PV <click4pv.work@gmail.com>
2018-10-26 08:54:04 +0100

    Initial Checkin

---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebAppSSO.Models;
+
+namespace WebAppSSO.Controllers
+{
+    /***** Begin Authorization Code Here *****/
+    [Authorize]
+    /***** End Authorization Code Here *****/
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppSSO.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
